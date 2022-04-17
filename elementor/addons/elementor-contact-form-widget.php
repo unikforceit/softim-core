11aa1c3ba0b8a22ffafba47bc80a8be703d04106
Md Rakibul Islam <irakibul189@gmail.com>
2022-04-17 13:34:30 +0600

    contact form widget

---
+++ elementor/addons/elementor-contact-form-widget.php
@@ -0,0 +1,326 @@
+<?php
+/**
+ * Elementor Widget
+ * @package Softim
+ * @since 1.0.0
+ */
+
+namespace Elementor;
+class Softim_Contact_Form_Widget extends Widget_Base
+{
+
+    /**
+     * Get widget name.
+     *
+     * Retrieve Elementor widget name.
+     *
+     * @return string Widget name.
+     * @since 1.0.0
+     * @access public
+     *
+     */
+    public function get_name()
+    {
+        return 'softim-contact-form-widget';
+    }
+
+    /**
+     * Get widget title.
+     *
+     * Retrieve Elementor widget title.
+     *
+     * @return string Widget title.
+     * @since 1.0.0
+     * @access public
+     *
+     */
+    public function get_title()
+    {
+        return esc_html__('Contact Form', 'softim-core');
+    }
+
+    /**
+     * Get widget icon.
+     *
+     * Retrieve Elementor widget icon.
+     *
+     * @return string Widget icon.
+     * @since 1.0.0
+     * @access public
+     *
+     */
+    public function get_icon()
+    {
+        return 'eicon-archive-title';
+    }
+
+    /**
+     * Get widget categories.
+     *
+     * Retrieve the list of categories the Elementor widget belongs to.
+     *
+     * @return array Widget categories.
+     * @since 1.0.0
+     * @access public
+     *
+     */
+    public function get_categories()
+    {
+        return ['softim_widgets'];
+    }
+
+    /**
+     * Register Elementor widget controls.
+     *
+     * Adds different input fields to allow the user to change and customize the widget settings.
+     *
+     * @since 1.0.0
+     * @access protected
+     */
+    protected function register_controls()
+    {
+
+        $this->start_controls_section(
+            'settings_section',
+            [
+                'label' => esc_html__('Graphic Image Section', 'softim-core'),
+                'tab' => Controls_Manager::TAB_CONTENT,
+            ]
+        );
+        $this->add_control(
+            'thumb_graphic1', [
+                'label' => esc_html__('Thumb Graphic Image 1', 'softim-core'),
+                'type' => Controls_Manager::MEDIA,
+                'show_label' => false,
+                'description' => esc_html__('thumb graphic image 1', 'softim-core'),
+                'default' => [
+                    'src' => Utils::get_placeholder_image_src()
+                ],
+            ]
+        );
+        $this->add_control(
+            'thumb_graphic2', [
+                'label' => esc_html__('Thumb Graphic Image 2', 'softim-core'),
+                'type' => Controls_Manager::MEDIA,
+                'show_label' => false,
+                'description' => esc_html__('thumb graphic image 2', 'softim-core'),
+                'default' => [
+                    'src' => Utils::get_placeholder_image_src()
+                ],
+            ]
+        );
+        $this->end_controls_section();
+
+
+        $this->start_controls_section(
+            'css_styles',
+            [
+                'label' => esc_html__('Styling Banner Content', 'softim-core'),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+        $this->add_control('contact_title_color', [
+            'label' => esc_html__('Title Color', 'softim-core'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => [
+                "{{WRAPPER}} .contact-item .contact-content .title" => "color: {{VALUE}}"
+            ]
+        ]);
+        $this->add_control('contact_info_color', [
+            'label' => esc_html__('Contact Info Color', 'softim-core'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => [
+                "{{WRAPPER}} .contact-item .contact-content p" => "color: {{VALUE}}"
+            ]
+        ]);
+        $this->add_control('contact_icon_bg', [
+            'label' => esc_html__('Icon Hover Background Color', 'softim-core'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => [
+                "{{WRAPPER}} .contact-item:hover .contact-icon" => "background-color: {{VALUE}}"
+            ]
+        ]);
+        $this->add_control('contact_icon_color', [
+            'label' => esc_html__('Contact Icon Color', 'softim-core'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => [
+                "{{WRAPPER}} .contact-item .contact-icon i" => "color: {{VALUE}}"
+            ]
+        ]);
+        $this->end_controls_section();
+
+        /* button styling */
+        $this->start_controls_section(
+            'banner_button_section',
+            [
+                'label' => esc_html__('Button Settings', 'softim-core'),
+                'tab' => Controls_Manager::TAB_STYLE
+            ]
+        );
+
+        $this->start_controls_tabs('button_styling');
+        $this->start_controls_tab('normal_style', [
+            'label' => esc_html__('Button Normal', "softim-core")
+        ]);
+        $this->add_control('button_normal_color', [
+            'label' => esc_html__('Button Text Color', 'softim-core'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => [
+                "{{WRAPPER}} .btn--base" => "color: {{VALUE}}"
+            ]
+        ]);
+        $this->add_control('divider_01', [
+            'type' => Controls_Manager::DIVIDER
+        ]);
+        $this->add_group_control(Group_Control_Background::get_type(), [
+            'name' => 'button_background',
+            'label' => esc_html__('Button Background ', 'softim-core'),
+            'selector' => "{{WRAPPER}} .btn--base"
+        ]);
+        $this->add_control('divider_02', [
+            'type' => Controls_Manager::DIVIDER
+        ]);
+        $this->add_group_control(Group_Control_Border::get_type(), [
+            'name' => 'header_button_border',
+            'label' => esc_html__('Border', 'softim-core'),
+            'selector' => "{{WRAPPER}} .btn--base"
+        ]);
+        $this->end_controls_tab();
+
+        $this->start_controls_tab('hover_style', [
+            'label' => esc_html__('Button Hover', "softim-core")
+        ]);
+        $this->add_control('button_hover_normal_color', [
+            'label' => esc_html__('Button Color', 'softim-core'),
+            'type' => Controls_Manager::COLOR,
+            'selectors' => [
+                "{{WRAPPER}} .btn--base:hover" => "color: {{VALUE}}"
+            ]
+        ]);
+        $this->add_control('divider_03', [
+            'type' => Controls_Manager::DIVIDER
+        ]);
+        $this->add_group_control(Group_Control_Background::get_type(), [
+            'name' => 'button_hover_background',
+            'label' => esc_html__('Button Background ', 'softim-core'),
+            'selector' => "{{WRAPPER}} .btn--base:hover"
+        ]);
+        $this->add_control('divider_04', [
+            'type' => Controls_Manager::DIVIDER
+        ]);
+        $this->add_group_control(Group_Control_Border::get_type(), [
+            'name' => 'header_hover_button_border',
+            'label' => esc_html__('Border', 'softim-core'),
+            'selector' => "{{WRAPPER}} .btn--base:hover"
+        ]);
+        $this->end_controls_tab();
+
+        $this->end_controls_tabs();
+
+        $this->add_control('divider_05', [
+            'type' => Controls_Manager::DIVIDER
+        ]);
+        $this->add_control(
+            'button_border_radius',
+            [
+                'label' => esc_html__('Border Radius', 'softim-core'),
+                'type' => Controls_Manager::DIMENSIONS,
+                'size_units' => ['px', '%'],
+                'selectors' => [
+                    '{{WRAPPER}} .btn--base' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                ],
+            ]
+        );
+        $this->end_controls_section();
+        /* button styling end */
+
+        /* typography settings start */
+        $this->start_controls_section('typography_settings', [
+            'label' => esc_html__('Typography Settings', 'softim-core'),
+            'tab' => Controls_Manager::TAB_STYLE
+        ]);
+        $this->add_group_control(Group_Control_Typography::get_type(), [
+            'name' => 'title_1_typography',
+            'label' => esc_html__('Title Typography', 'softim-core'),
+            'selector' => "{{WRAPPER}} .contact-item .contact-content .title"
+        ]);
+        $this->add_group_control(Group_Control_Typography::get_type(), [
+            'name' => 'info_typography',
+            'label' => esc_html__('Info Typography', 'softim-core'),
+            'selector' => "{{WRAPPER}} .contact-item .contact-content p"
+        ]);
+        $this->add_group_control(Group_Control_Typography::get_type(), [
+            'name' => 'button_typography',
+            'label' => esc_html__('Icon Typography', 'softim-core'),
+            'selector' => "{{WRAPPER}} .contact-item .contact-icon"
+        ]);
+        $this->end_controls_section();
+        /* typography settings end */
+
+    }
+
+    /**
+     * Render Elementor widget output on the frontend.
+     *
+     * Written in PHP and used to generate the final HTML.
+     *
+     * @since 1.0.0
+     * @access protected
+     */
+    protected function render()
+    {
+        $settings = $this->get_settings_for_display();
+        $graphic1 = $settings['thumb_graphic1']['url'];
+        $graphic2 = $settings['thumb_graphic2']['url'];
+
+        ?>
+
+        <section class="contact-section ptb-120">
+            <div class="contact-element-one">
+                <img src="<?php echo esc_html($graphic1);?>" alt="element">
+            </div>
+            <div class="contact-element-two">
+                <img src="<?php echo esc_html($graphic2);?>" alt="element">
+            </div>
+            <div class="container">
+                <form class="contact-form">
+                    <div class="row justify-content-center mb-25-none">
+                        <div class="col-xl-6 col-lg-6 form-group">
+                            <label>Enter Name</label>
+                            <input type="text" name="name" class="form--control" placeholder="Jhon david smith">
+                        </div>
+                        <div class="col-xl-6 col-lg-6 form-group">
+                            <label>Email Address</label>
+                            <input type="email" name="email" class="form--control" placeholder="jhonsmith@gmail.com">
+                        </div>
+                        <div class="col-xl-6 col-lg-6 form-group">
+                            <label>Your Phone</label>
+                            <input type="text" name="number" class="form--control" placeholder="+1 (900) 696 3600" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');" />
+                        </div>
+                        <div class="col-xl-6 col-lg-6 form-group">
+                            <label>Select Subject</label>
+                            <div class="contact-select">
+                                <select class="form--control">
+                                    <option value="1">Web Development</option>
+                                    <option value="2">Web Design</option>
+                                    <option value="3">Digital Marketing</option>
+                                    <option value="4">Search SEO</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-xl-12 col-lg-12 form-group">
+                            <label>Write Message</label>
+                            <textarea name="message" class="form--control" placeholder="Write Here ..."></textarea>
+                        </div>
+                        <div class="col-xl-12 col-lg-12 form-group text-center">
+                            <button type="submit" class="btn--base mt-20">Send Now <i class="fas fa-arrow-right ml-2"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <?php
+    }
+}
+
+Plugin::instance()->widgets_manager->register(new Softim_Contact_Form_Widget());
